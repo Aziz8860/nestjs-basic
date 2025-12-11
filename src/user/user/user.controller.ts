a6f88b94384e323acf15eecbab54a7edcad7f1f8
ariqaziz <muhaariq@gmail.com>
2025-12-11 16:40:00 +0700

    feat: view

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -5,6 +5,14 @@ import type { Request, Response } from 'express';
 @Controller('/api/users')
 export class UserController {
 
+    @Get('/view/hello')
+    viewHello(@Query('name') name: string, @Res() response: Response) {
+        response.render('index.html', {
+            title: 'Template Engine',
+            name: name,
+        })
+    }
+
     @Get('/set-cookie')
     setCookie(@Query('name') name: string, @Res() response: Response) {
         response.cookie('name', name);
