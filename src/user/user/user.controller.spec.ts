d54cb7bd9b440bc2a6765f0ee14bc3ba66e59ce0
ariqaziz <muhaariq@gmail.com>
2025-12-11 16:57:22 +0700

    feat: unit test

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,12 +8,26 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      imports: [], // masukin module lain di sini
+      providers: [], // masukin provider lain di sini
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Ariq', 'Aziz');
+    expect(response).toBe('Hello Ariq Aziz');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Ariq', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      title: 'Template Engine',
+      name: 'Ariq',
+    });
   });
 });
